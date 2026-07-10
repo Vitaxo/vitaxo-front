5cc66f67753d5d54e1c28f5fca97c613aee1f758
Sagbyy <sbouhdjeur.gagny@gmail.com>
2026-07-10 18:14:58 +0200

    Add Sentry for error tracking and update dependencies in package.json and pnpm-lock.yaml

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { App } from ".";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://801ac57e062e7783ad44b42a6a0ef7bf@o4511711539363840.ingest.de.sentry.io/4511711861276752",
+  dataCollection: {
+  }
+});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
